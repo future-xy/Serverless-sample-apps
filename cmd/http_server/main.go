05e2bb604e3a531310fde3a381568eb085f543e3
future-xy <fuyao3860@gmail.com>
2022-08-30 23:13:58 +0100

    add latency

--- cmd/http_server/main.go
+++ cmd/http_server/main.go
@@ -1,13 +1,20 @@
 package main
 
-import "fmt"
-import "os"
-import "net/http"
-import "github.com/labstack/echo/v4"
-import "github.com/labstack/echo/v4/middleware"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
 func main() {
 	fmt.Println("Hello! This is a sample application to test serverless")
+
+	var sleepTime int = 0
 
 	e := echo.New()
 	e.Use(middleware.Logger())
@@ -18,9 +25,26 @@ func main() {
 	})
 
 	e.GET("/ping", func(c echo.Context) error {
+		time.Sleep(time.Duration(sleepTime) * time.Millisecond)
 		return c.JSON(http.StatusOK, struct {
-			Status string
-		}{Status: "OK"})
+			Status    string
+			SleepTime int
+		}{Status: "OK", SleepTime: sleepTime})
+	})
+
+	e.POST("/sleep", func(c echo.Context) error {
+		newSleepTime, err := strconv.Atoi(c.FormValue("time"))
+		if err != nil {
+			return c.JSON(http.StatusOK, struct {
+				Status  string
+				Success string
+			}{Status: "OK", Success: "False"})
+		}
+		sleepTime = newSleepTime
+		return c.JSON(http.StatusOK, struct {
+			Status  string
+			Success string
+		}{Status: "OK", Success: "True"})
 	})
 
 	httpPort := os.Getenv("HTTP_PORT")
