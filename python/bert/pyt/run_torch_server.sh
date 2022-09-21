dad293175e4aadfb59367e146f064080282ed50f
future-xy <fuyao3860@gmail.com>
2022-09-21 21:26:58 +0100

    add docker build

--- python/bert/pyt/run_torch_server.sh
+++ python/bert/pyt/run_torch_server.sh
@@ -5,3 +5,4 @@ mv bert-pyt.mar model_store/
 torchserve --start --ncs --model-store model_store --models bert=bert-pyt.mar
 
 # curl -X POST http://127.0.0.1:8080/predictions/bert -T ./input.json
+# ./build_image.sh -g -cv cu113 -t futurexy/torchserve:latest
